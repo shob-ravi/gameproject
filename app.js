974e277f45633d5516bdacb19b079d259f511205
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-15 21:05:17 -0500

    created div for grid

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 let memoryGameContainerEl = document.querySelector(".memoryGameContainer");
+for(let i=1;i<=16;i++)
+    {
+        let gridItem = document.createElement('div');        
+        
+    }
