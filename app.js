58fd9cbb742b5227df71b29a21c00ac2a69f0670
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-15 21:14:34 -0500

    added text content

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ for(let i=1;i<=16;i++)
         let gridItem = document.createElement('div');        
         memoryGameContainerEl.appendChild(gridItem);
         gridItem.classList.add('grid-item');
+        gridItem.textContent=i;
     }
