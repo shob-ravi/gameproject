6bd6db5fd3c9055f78a1be61382dd7a9a8c9eb60
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-15 21:28:08 -0500

    added one image file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let image1 = '<img src="https://media.gettyimages.com/id/1311529685/vector/abstract-retro-logos.jpg?s=612x612&w=gi&k=20&c=Tm_64SScULgt-q-1bOUHaEuCbeiZF1exwn3FHiomgao=" width=10%;/>';
 let memoryGameContainerEl = document.querySelector(".memoryGameContainer");
 memoryGameContainerEl.classList.add('grid-container');
 for(let i=1;i<=16;i++)
@@ -6,4 +7,12 @@ for(let i=1;i<=16;i++)
         memoryGameContainerEl.appendChild(gridItem);
         gridItem.classList.add('grid-item');
         gridItem.textContent=i;
+        gridItem.addEventListener('click',()=>{
+            gridItem.textContent=image1;
+        });
+        
+    }
+    showPic()
+    {
+        gridItem.textContent="https://media.gettyimages.com/id/1311529685/vector/abstract-retro-logos.jpg?s=612x612&w=gi&k=20&c=Tm_64SScULgt-q-1bOUHaEuCbeiZF1exwn3FHiomgao=";
     }
