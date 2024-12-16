50931cf01b66321f1ea7f3b778ecdef4862621da
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-15 23:05:38 -0500

    changed the for loop to for each loop

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-let image1 = "https://e1.pngegg.com/pngimages/457/896/png-clipart-icon-no-background-icon-512x512-2x-color-shade-fan.png";
-let image2="https://www.freeiconspng.com/thumbs/color-icons/colors-icon-4.png";
+// let image1 = "https://e1.pngegg.com/pngimages/457/896/png-clipart-icon-no-background-icon-512x512-2x-color-shade-fan.png";
+// let image2="https://www.freeiconspng.com/thumbs/color-icons/colors-icon-4.png";
+let imageArray = ["https://e1.pngegg.com/pngimages/457/896/png-clipart-icon-no-background-icon-512x512-2x-color-shade-fan.png",
+    "https://www.freeiconspng.com/thumbs/color-icons/colors-icon-4.png"
 
+]
 let memoryGameContainerEl = document.querySelector(".memoryGameContainer");
 memoryGameContainerEl.classList.add('grid-container');
-for (let i=1;i<=16;i++)
-    {
+imageArray.forEach((image)=>{
+    
         let gridItem = document.createElement('div');        
         memoryGameContainerEl.appendChild(gridItem);
         gridItem.classList.add('grid-item');
-        // gridItem.textContent=i;
+        
 
-        const img2 = document.createElement('img');
-        gridItem.appendChild(img2);
-
+        const img = document.createElement('img');
+        gridItem.appendChild(img);
+        console.log(image);
         gridItem.addEventListener('click',() => {
-        img2.src = image2;
-        });
-        
-    }
-    showPic()
-    {
-        
-    }
+           img.src = image;
+           
+});
+});        
