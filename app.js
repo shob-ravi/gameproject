3bf844b69040632e2d89824e5a8cb0ecbb76c416
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-15 21:12:57 -0500

    added style for grid container

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ for(let i=1;i<=16;i++)
     {
         let gridItem = document.createElement('div');        
         memoryGameContainerEl.appendChild(gridItem);
+        gridItem.classList.add('grid-item');
     }
