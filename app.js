a8ada3d75a5b97dce6df8527438901072c599c8c
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-15 21:06:49 -0500

    appendchild

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ let memoryGameContainerEl = document.querySelector(".memoryGameContainer");
 for(let i=1;i<=16;i++)
     {
         let gridItem = document.createElement('div');        
-        
+        memoryGameContainerEl.appendChild(gridItem);
     }
